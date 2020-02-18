1e5b2dba869810a830544355012234f757f73d58
unknown <saidaneaymeen@gmail.com>
2020-02-18 14:56:44 +0100

    signin

---
+++ assets/javascripts/pages-signin.js
@@ -0,0 +1,20 @@
+function login() {
+  var object = JSON.parse(localStorage.getItem("user")) || [];
+  var user = document.getElementById("username").value;
+  var pwd = document.getElementById("password").value;
+  var connect = false;
+  var connecteduser;
+
+  for (let i = 0; i < object.length; i++) {
+    if (object[i].username == user && object[i].password == pwd) {
+      connect = true;
+      connecteduser = object[i]; 
+    }
+  }
+  if (connect == true) {
+    
+    localStorage.setItem("connecteduser", JSON.stringify(connecteduser));
+  } else {
+    alert("Fausses Informations");
+  } 
+}
--- assets/javascripts/pages-signup.js
+++ assets/javascripts/pages-signup.js
@@ -1,28 +1,26 @@
 var username = document.getElementById("username");
-    var password=document.getElementById("password");
-    var email=document.getElementById("email");
-    
-    
-function newuser(){
-    
-    var neww = JSON.parse(localStorage.getItem('user')) || [];
-    
-   var userid= Math.floor(Math.random() * 1000); 
+var password = document.getElementById("password");
+var email = document.getElementById("email");
 
+function newuser() {
+  var neww = JSON.parse(localStorage.getItem("user")) || [];
 
-    if ((username.value === '')&& (password.value=='')&&(email.value=='')) {
-       alert("You must write something!");
-     } else {
-      var text = {
-         id: userid,
-         role:"",
-         status:"",
-   username:username.value,
-   password:password.value,
-   email:email.value }
-      neww.push(text);
-     
-   localStorage.setItem('user', JSON.stringify(neww));
-    
-   }
-   }
+  var userid = Math.floor(Math.random() * 1000);
+
+  if (username.value === "" && password.value == "" && email.value == "") {
+    alert("You must write something!");
+  } else {
+    var text = {
+      id: userid,
+      role: "",
+      status: "",
+      username: username.value,
+      password: password.value,
+      email: email.value
+    };
+    neww.push(text);
+
+    localStorage.setItem("user", JSON.stringify(neww));
+  }
+}
+
